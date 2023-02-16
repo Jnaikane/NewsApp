1517e8ee7d7bb76cd9529da2609bde14912064e3
JN62847 <jnaikane@gmail.com>
2023-02-16 12:31:44 +0530

    initial commit

---
+++ app/src/main/java/com/codingtest/newsapp/constants/Constants.java
@@ -0,0 +1,9 @@
+package com.codingtest.newsapp.constants;
+
+public class Constants {
+
+        public static final String API_KEY = "94c37d7a040e49d8909f0382dcbbd5b7";
+        public static final String API_BASE_URL = "https://newsapi.org/v2/top-headlines?country=in&apiKey="+API_KEY;
+
+
+}
